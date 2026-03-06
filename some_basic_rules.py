5cd26c5934a9d44b1ea06e963ca1e0cc7c737503
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-06 22:42:01 +0530

    Create some_basic_rules.py
    These are some basic rules we have to always check for

---
+++ some_basic_rules.py
@@ -0,0 +1,11 @@
+a=23
+aaa=243
+harry=33
+sandy=66
+_vishu=77
+@vishu=77#wrong
+
+
+# except_ we cant use any symobls....@is also banned we cant evn use space 
+
+# a variable can never start with a integer...it should alwaysn start withn a name or a underscore
