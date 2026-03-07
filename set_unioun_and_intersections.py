f26f959019560749e1525ca36b6d95da75639e2b
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-07 21:04:09 +0530

    Set union and intersection
    Set union and intersection

---
+++ set_unioun_and_intersections.py
@@ -0,0 +1,10 @@
+s1={1,2,3,4,324}
+s2={3,4,5,6,7,8,9}
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.difference(s2))
+print(s2.difference(s1))
+print(s1.symmetric_difference(s2))
+print(s1.issubset(s2))
+print(s1.issuperset(s2))
+#this was the code for union and intersection of sets
