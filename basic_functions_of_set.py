e795d2a6b3862fb05fd8cb04bd1c5c0813058d85
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-07 21:00:36 +0530

    Create basic_functions_of_set.py
    These are some Basic functions of sets

---
+++ basic_functions_of_set.py
@@ -0,0 +1,9 @@
+#For making an empty set empty_set = set()
+#dont use empty_set = {},as it is gonna create a dictionary
+s={2,3,4,1,5,5,5,5,78,90,}
+print(s)
+#s.add(100) #adds 100 to the set
+s.add(837837)
+print(s)
+s.remove(90)  #removes 90 from the set
+print(s)    
