c556d6e2e21171446c9f090a578b9c7a608ca91c
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-07 21:01:47 +0530

    Create printing_length_of_set.py
    This code is to print length of a set

---
+++ printing_length_of_set.py
@@ -0,0 +1,9 @@
+s=set()
+s.add(20)
+s.add(20.0)
+s.add("241")
+print(len(s))
+#if 1==1.00 # this will return true because 1 and 1.00 are same
+# if 1==1.0 # this will return true because 1 and 1.0 are same
+#if 1==1.2 # this will return false because 1 and 1.2 are not same
+# if 1==1.000 # this will return true because 1 and 1
