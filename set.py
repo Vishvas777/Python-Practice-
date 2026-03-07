354d0ebc316173faff332d7a0b7f940952502e9d
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-07 21:03:33 +0530

    Create set.py
    This code is for adding a number in set or deleting from the set.Here set is also defined by user.

---
+++ set.py
@@ -0,0 +1,16 @@
+s=set()
+n=input("enter a number: ")
+s.add(int(n))
+n=input("enter a number: ")
+s.add(int(n))
+n=input("enter a nu mber: ")
+s.add(int(n))
+n=input("enter a number: ")
+s.add(int(n))
+n=input("enter a number: ")
+s.add(int(n))
+print(s)
+
+
+# This code snippet collects five numbers from the user and adds them to a set.# Sets automatically handle duplicates, so if the user enters the same number multiple times, it will only be stored once in the set. The final set is printed, showing all unique numbers entered by the user.
+# Note: The code snippet provided is a simple example of how to use sets in Python                  
