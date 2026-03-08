bf4ff6f975e7f309239bc19321c0ea8024c2a89d
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-08 23:32:05 +0530

    Create basic_if_and_else.py

---
+++ basic_if_and_else.py
@@ -0,0 +1,12 @@
+a=int(input("enter a number:"))
+if(a)>=18:
+    print("you are good for this programing softwaare")
+    print("have a nice day ")  
+elif(a<0):print("why are you enetering a wrong age what happend to you bro?? did you ever even heard about negative age or what??") 
+
+elif(a==0):print("i really dont wanna be rude today who has a age of 0??)")
+else:
+    print("you need to learn alot to enther this software")
+    print("i am sorry but curremtly i cant help you in this")  
+
+print("thank you for using this software")
