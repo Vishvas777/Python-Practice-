3c8827999b65c1af620e8034a3c3094be84f2c55
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-08 23:32:09 +0530

    Create multiple_if_statements.py

---
+++ multiple_if_statements.py
@@ -0,0 +1,24 @@
+a=int(input("enter a numer:"))
+
+#if statement no 1
+if(a)%2==0:
+    print("even number")
+
+#if statement 1 ends here
+# if statement no 2
+if(a)%2!=0:
+    print("odd number")
+#if statement 2 ends here
+
+if(a)>=18:
+    print("you are good for this programing softwaare")
+    print("have a nice day ")  
+elif(a<0):print("why are you enetering a wrong age what happend to you bro?? did you ever even heard about negative age or what??") 
+
+elif(a==0):print("i really dont wanna be rude today who has a age of 0??)")
+
+else:
+    print("you need to learn alot to enther this software")
+    print("i am sorry but curremtly i cant help you in this")  
+
+print("thank you for using this software")
