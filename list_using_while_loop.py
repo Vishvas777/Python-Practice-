a1db5e587f8560b52884cc7b0ebf4215235be1b2
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-09 11:03:00 +0530

    Create list_using_while_loop.py

---
+++ list_using_while_loop.py
@@ -0,0 +1,9 @@
+l=[1,"harry",3.4,True,"reeva""swareena"]
+
+i=0
+while i<len(l):
+    print(l[i])
+    i+=1
+# This is a while loop that will print elements of the list l one by one
+# The loop will continue as long as the condition (i<len(l)) is true
+# Since i starts at 0 and increments by 1 each iteration, it will eventually reach the length of the list, at which point the loop will stop
