627c17aa1fd9da2315e453b517d491c20390661c
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-09 11:02:52 +0530

    Create printing_numbers_without_loop.py

---
+++ printing_numbers_without_loop.py
@@ -0,0 +1,12 @@
+print(1)
+print(2)
+print(3)            
+print(4)
+print(5)    
+print(6)
+
+
+#instead of writing print statements one by one, we can use a loop
+ 
+for i in range (1,154185):
+    print(i)
