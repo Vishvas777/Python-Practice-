146e03d8775a76b7b806d7eeb78e9545009dbc15
Vyamsani Sai Vishvas <vyamsanisaivishvas@gmail.com>
2026-03-12 09:15:59 +0530

    Create simple_intrest.py

---
+++ simple_intrest.py
@@ -0,0 +1,5 @@
+principal_amt=int(input("enter the principle amount:"))
+rate=float(input("enter the rate of intrest:"))
+time=int(input("enter the time required:"))
+simple_intrest=(principal_amt*rate*time)/100
+print("simple intrest",simple_intrest)
